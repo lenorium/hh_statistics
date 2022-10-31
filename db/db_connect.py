8f9e8e557ceb1876affd4d411061a3fadc7b6ad8
lenorium <lenorium>
2022-10-31 17:12:32 +0200

    настройки для хероку

--- db/db_connect.py
+++ db/db_connect.py
@@ -11,17 +11,27 @@ class DbConnection:
 
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
-            database = {
-                'drivername': 'postgresql+psycopg2',
-                'host': config.POSTGRES_HOST,
-                'port': config.POSTGRES_PORT,
-                'username': config.POSTGRES_USER,
-                'password': config.POSTGRES_PASSWORD,
-                'database': config.POSTGRES_DB
-            }
-
             cls._instance = super(DbConnection, cls).__new__(cls)
-            cls._instance.engine = create_engine(URL(**database), echo=config.POSTGRES_LOG)
+
+            # если запускаем на heroku, то используется перем. DATABASE_URL
+            if config.DATABASE_URL:
+                connect_url = config.DATABASE_URL
+                if connect_url.startswith("postgres://"):
+                    connect_url = connect_url.replace("postgres://", "postgresql://", 1)
+            else:
+                database = {
+                    'drivername': 'postgresql+psycopg2',
+                    'host': config.POSTGRES_HOST,
+                    'port': config.POSTGRES_PORT,
+                    'username': config.POSTGRES_USER,
+                    'password': config.POSTGRES_PASSWORD,
+                    'database': config.POSTGRES_DB
+                }
+                connect_url = URL(**database)
+
+            cls._instance.engine = create_engine(connect_url,
+                                                 sslmode='require',  # для heroku
+                                                 echo=config.POSTGRES_LOG)
             cls._instance.session_maker = sessionmaker(bind=cls._instance.engine)
             Base.metadata.create_all(cls._instance.engine)
         return cls._instance
